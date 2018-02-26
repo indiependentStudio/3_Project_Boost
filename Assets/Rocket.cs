4f3dc6eae8038233e81188824c28d0582b8e2f46
Dave Megarry <dave@poweredbypixiedust.uk>
2018-02-26 21:35:40 +0000

    Tweaked rotation and thrust for better feel.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,6 +2,9 @@
 
 public class Rocket : MonoBehaviour
 {
+	[SerializeField] private float rcsThrust = 100f;
+	[SerializeField] private float thrust = 10f;
+	
 	private Rigidbody _rigidbody;
 	private AudioSource _audioSource;
 	
@@ -15,31 +18,40 @@ public class Rocket : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		ProcessInput();
+		Rotate();
+		Thrust();
 	}
 
-	private void ProcessInput()
+	private void Rotate()
+	{
+		_rigidbody.freezeRotation = true; // switch off physics rotation
+
+		float rotationThsFrame = rcsThrust * Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * rotationThsFrame);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward * rotationThsFrame);
+		}
+
+		_rigidbody.freezeRotation = false; // relinquish manual control of rotation
+	}
+
+	private void Thrust()
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
 			if (!_audioSource.isPlaying)
 				_audioSource.Play();
-			
-			_rigidbody.AddRelativeForce(Vector3.up);
+
+			_rigidbody.AddRelativeForce(Vector3.up * thrust);
 		}
 		else if (_audioSource.isPlaying)
 		{
 			_audioSource.Stop();
-		}
-
-		if (Input.GetKey(KeyCode.A))
-		{
-//			print(Time.deltaTime);
-			transform.Rotate(Vector3.forward);
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			transform.Rotate(-Vector3.forward);
 		}
 	}
 }
