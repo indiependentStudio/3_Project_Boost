21fe6965cbb695d1e0cd9d52771b973150e69085
Dave Megarry <dave@poweredbypixiedust.uk>
2018-02-26 16:50:57 +0000

    Rocket basic movement and sound.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Rocket : MonoBehaviour {
-
+public class Rocket : MonoBehaviour
+{
+	private Rigidbody _rigidbody;
+	private AudioSource _audioSource;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		_rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			if (!_audioSource.isPlaying)
+				_audioSource.Play();
+			
+			_rigidbody.AddRelativeForce(Vector3.up);
+		}
+		else if (_audioSource.isPlaying)
+		{
+			_audioSource.Stop();
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+//			print(Time.deltaTime);
+			transform.Rotate(Vector3.forward);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward);
+		}
 	}
 }
